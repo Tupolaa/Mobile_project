01f1700c935b286843d2f95e32fa914b037c23f3
Joni Mitronen <joni.mitronen@student.hamk.fi>
2025-09-30 17:21:42 +0300

    reviewit user id ja tokenin mukaa toimii backendAPI tiedoston kautta

--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -1,39 +1,51 @@
-import React, { useContext, useState } from "react";
-import { View, Button, Text } from "react-native";
+import React, { useContext, useState, useEffect } from "react";
+import { View, Button, FlatList } from "react-native";
 import { AuthContext } from "../context/AuthContext";
-import { BACKEND_URL } from "@env";
+import { getAllReviewsByUser } from "../services/backendAPI";
+import ReviewItem from "../components/ReviewItem";
+import useBottomPadding from "../hooks/useBottomPadding";
 
 export default function TestScreen({ navigation }) {
+  const contentPadding = useBottomPadding();
   const { token, user, logout } = useContext(AuthContext);
-  const [response, setResponse] = useState("");
+  const [reviews, setReviews] = useState([]);
 
-  const testUserReviews = async () => {
+  const loadReviews = async () => {
     if (!token) {
       setResponse("No token found!");
       return;
     }
     try {
-      // console.log(BACKEND_URL);
-      //user.id comes from AuthContext user state
-      const res = await fetch(`${BACKEND_URL}/review/user/${user.id}`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      console.log(res);
-      const data = await res.json();
+      // get all testing
+      const rows = await getAllReviewsByUser(token, user.id);
+      // get by movie id testing
+      // const movieId = "680d29f98e8db1ede3dfa796";
+      // const rows = await getAllReviewsByMovie(movieId);
 
-      setResponse(JSON.stringify(data, null, 2));
+      setReviews(rows);
     } catch (err) {
-      setResponse("Error: " + err.message);
+      console.error(err);
+      setReviews(err);
     }
   };
+
+  useEffect(() => {
+    loadReviews();
+  }, []);
 
   return (
     <View>
       <Button title="Go to Login" onPress={() => navigation.navigate("Login")} />
       <Button title="Go to Register" onPress={() => navigation.navigate("Register")} />
-      <Button title="Get My Reviews" onPress={testUserReviews} />
+      <Button title="Get My Reviews" onPress={loadReviews} />
       <Button title="Logout" onPress={logout} color="red" />
-      <Text>{response}</Text>
+
+      <FlatList
+        data={reviews}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => <ReviewItem item={item} />}
+        contentContainerStyle={contentPadding}
+      />
     </View>
   );
 }
